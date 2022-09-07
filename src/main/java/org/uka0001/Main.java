1cd1349c5165eb5ccde479afe75542255adc4d20
Uka0001 <ukraine0001@gmail.com>
2022-09-07 12:49:22 +0300

    start init, need fix

---
+++ src/main/java/org/uka0001/Main.java
@@ -0,0 +1,61 @@
+package org.uka0001;
+
+import jdk.internal.org.objectweb.asm.TypeReference;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList<>();
+        TypeReference <Integer> token = new TypeReference() {}
+        System.out.println(populate(list));
+    }
+
+    static Map<Class<?>, Supplier<Object>> generator = new LinkedHashMap<>();
+    static List<String> list = new ArrayList<>();
+    TypeReference <Map<String, Integer>> typeReference = new TypeReference<Map<String, Integer>>();
+
+
+    static {
+        generator.put(Integer.class, () -> 1);
+        generator.put(Boolean.class, () -> true);
+        generator.put(String.class, () -> "Hello");
+    }
+
+    private static <T> Object populate(ArrayList<String> x) {
+        if (isSimpleType(x)) {
+            return generator.get(x).get();
+        } else {
+            if (isCollection(x.getClass())) {
+                if(List.class.isAssignableFrom(x.getClass())){
+
+                }
+            } else {
+                //TODO cought Class
+            }
+
+        }
+        return null;
+    }
+
+    private static boolean isCollection(Class<?> x) {
+        return List.class.isAssignableFrom(x) || Map.class.isAssignableFrom(x);
+    }
+
+    private static boolean isSimpleType(Object x) {
+        return generator.containsKey(x);
+    }
+
+    private <T> Type[] nestedTypes (Type typeRef) {
+        return typeRef instanceof ParameterizedType ?
+                ((ParameterizedType) typeRef)
+                        .getActualTypeArguments() : new Type[] {typeRef};
+
+    }
+}
---
+++ src/main/java/org/uka0001/X.java
@@ -0,0 +1,15 @@
+package org.uka0001;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class X {
+    String name;
+    List <X> list;
+    Boolean flag;
+
+}
